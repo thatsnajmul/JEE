8d3b749a8a59e5b81d54551dbf8d899483c54fec
thatsnajmul <thatsnajmul@yahoo.com>
2024-03-07 11:37:45 +0600

    Update

--- Module 02/Javascript/Class-9/form.js
+++ Module 02/Javascript/Class-9/form.js
@@ -9,9 +9,20 @@ function submitForm(event) {
     let pass = document.getElementById('pass').value;
 
 
-    let gender=document.querySelector('input[name="gender"]:checked').value;
-    // let hobby=document.querySelectorAll('input[name="hobby"]:checked').value;
-    // let course=document.querySelector('input[name="course"]:checked').value;
+    let gender = document.querySelector('input[name="gender"]:checked').value;
+
+
+    let hobby = document.querySelectorAll('input[name="hobby"]:checked');
+
+    let hoobbyValue = [];
+
+    hobby.forEach(
+        function (hobby) {
+            hoobbyValue.push(hobby.value);
+        }
+    );
+
+    let course = document.getElementById('course').value;
 
     // let gender="";
     // let genderElements = document.getElementsByName('gender');
@@ -21,12 +32,15 @@ function submitForm(event) {
     //     }
     // }
 
-    // let hobArray = [];
+    // let hobbyArray = [];
     // let hobbyElements = document.getElementsByName('hobby');
+    // for (let i = 0; i < hobbyElements.length; i++) {
+    //     if (hobbyElements[i].checked) {
+    //         hobbyArray.push(hobbyElements[i].value);
+    //     }
+    //keep
 
-    // let course = document.getElementById('course').value;
 
-    
     let dob = document.getElementById('dob').value;
     let comment = document.getElementById('comment').value;
 
@@ -35,8 +49,8 @@ function submitForm(event) {
     output += "Phone: " + phone + "\n";
     output += "Pass: " + pass + "\n";
     output += "Gender: " + gender + "\n";
-    // output += "Course: " + course + "\n";
-    // output += "Hobby: " + hobby + "\n";
+    output += "Course: " + course + "\n";
+    output += "Hobby: " + hoobbyValue + "\n";
     output += "DOB: " + dob + "\n";
     output += "Comment: " + comment + "\n";
 
