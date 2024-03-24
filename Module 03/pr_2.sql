c68443fdacf6aeec79520efeb23f0f2b70d3d6bd
thatsnajmul <thatsnajmul@yahoo.com>
2024-03-24 16:30:31 +0600

    Update pr_2.sql

--- Module 03/pr_2.sql
+++ Module 03/pr_2.sql
@@ -43,6 +43,91 @@ where first_name like '%&first_name%'
 order by first_name desc
 ;
 
+--Initcap
+ select first_name, last_name, salary
+ from employees
+ where first_name = INITCAP('Payam');
+ 
+--lower
+select first_name, last_name, salary
+ from employees
+ where lower(first_name) = 'payam';
+ 
+ --Initcap
+ 
+ 
+ select first_name, last_name, salary,
+ concat(first_name, last_name) as "Full Name", substr(first_name,3,3) as "Substring"
+ from employees
+ where first_name = initcap('payam'); --initcap find a specific data where
+ 
+ --Substring
+ select first_name , substr(first_name,1,3) as "SubStr S"
+ from employees;
+ 
+ 
+ --length
+ select first_name, last_name, salary,
+ concat(first_name, last_name) as "Full Name", substr(first_name,3,3) as "Substring", length(first_name)
+ from employees;
+ 
+ --Instr
+ 
+select first_name, last_name, salary,
+ concat(first_name, last_name) as "Full Name", substr(first_name,3,3) as "Substring", instr(first_name, 'l')
+ from employees;
+ 
+ select first_name,last_name, instr(first_name, 'a')
+ from employees;
+ 
+select first_name,last_name, instr(lower(first_name), 'a')
+from employees;
+
+--Lpad
+select first_name,last_name, lpad(first_name, '10', '-')
+from employees;
+
+--Rpad
+select first_name,last_name, rpad(first_name, '10', '-')
+from employees;
+
+
+select concat(concat(last_name, '''s job category is '), job_id)
+"Job" from employees
+where substr(job_id, 4) = 'REP';
+
+
+
+
+
+select first_name, last_name, job_id
+from employees
+where substr(job_id, 1,2) =  'IT';
+ 
+
+--Er
+select first_name, last_name, job_id,
+concat(substr(first_name, 2,2),lower(substr(job_id, 1,2)))
+from employees;
+
+
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+
  
 
 
