0e3aa9db3d9f9fbf10f0a68aa48424debf9212b4
thatsnajmul <thatsnajmul@yahoo.com>
2024-04-02 16:38:38 +0600

    uip

--- Module 03/up.sql
+++ Module 03/up.sql
@@ -8,7 +8,7 @@ where job_id='SA_MAN' or job_id='IT_PROG';
 select * from employees
 where employee_id='150' or employee_id='160';
 
-select job_title 
+select job_title,max_salary-min_salary as Count_Salary
 from jobs
 where max_salary>=10000 and min_salary<=20000;
 
