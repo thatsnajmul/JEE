dc0c6acf3c40c03441a2e7afb3d08a47210366e9
thatsnajmul <thatsnajmul@yahoo.com>
2024-04-16 15:30:29 +0600

    Update

--- Module 03/mid_ex.sql
+++ Module 03/mid_ex.sql
@@ -164,4 +164,29 @@ from employees e
 join employees es on e.department_id=es.department_id
 where e.employee_id <> es.employee_id
 order by 1;
-        
+
+--============================
+--Job Grades Data insert start
+--============================
+CREATE TABLE job_grades (
+grade 		CHAR(1),
+lowest_sal 	NUMBER(8,2) NOT NULL,
+highest_sal	NUMBER(8,2) NOT NULL
+);
+
+ALTER TABLE job_grades
+ADD CONSTRAINT jobgrades_grade_pk PRIMARY KEY (grade);
+
+INSERT INTO job_grades VALUES ('A', 1000, 2999);
+INSERT INTO job_grades VALUES ('B', 3000, 5999);
+INSERT INTO job_grades VALUES ('C', 6000, 9999);
+INSERT INTO job_grades VALUES ('D', 10000, 14999);
+INSERT INTO job_grades VALUES ('E', 15000, 24999);
+INSERT INTO job_grades VALUES ('F', 25000, 40000);
+
+COMMIT
+--==========================
+--Job Grade data insert end
+--==========================
+
+
