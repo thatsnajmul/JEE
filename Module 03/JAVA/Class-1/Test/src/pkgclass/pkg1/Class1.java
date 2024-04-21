cd8f29ac32a5220c79535d04ced6a2f15db9cdd6
thatsnajmul <thatsnajmul@yahoo.com>
2024-04-21 23:47:53 +0600

    Update

--- Module 03/JAVA/Class-1/Test/src/pkgclass/pkg1/Class1.java
+++ Module 03/JAVA/Class-1/Test/src/pkgclass/pkg1/Class1.java
@@ -15,7 +15,7 @@ public class Class1 {
      */
     public static void main(String[] args) {
         // TODO code application logic here        
-        System.out.println("Hello World Welcome to java class");
+        System.out.println("Hello World! Welcome to java class....");
     }
     
 }
