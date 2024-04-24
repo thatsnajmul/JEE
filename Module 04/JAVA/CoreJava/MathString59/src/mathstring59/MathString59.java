26e8dc1e8637c94d37003e15f481e5aadbff6617
thatsnajmul <thatsnajmul@yahoo.com>
2024-04-24 18:18:23 +0600

    Update Java Math built in class
    Update Java Math built in class that declare java previously in their system.

---
+++ Module 04/JAVA/CoreJava/MathString59/src/mathstring59/MathString59.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package mathstring59;
+
+public class MathString59 {
+
+    public static void main(String[] args) {
+        
+        System.out.println(Math.abs(-2.50));
+        System.out.println(Math.random());
+        System.out.println(Math.max(50,10));
+        System.out.println(Math.min(50,10));
+        System.out.println(Math.round(5.54));
+        System.out.println(Math.ceil(5.54));
+        System.out.println(Math.floor(5.54));
+        System.out.println(Math.log10(5));
+        System.out.println(Math.pow(5,3));
+        System.out.println(Math.sqrt(64));
+        System.out.println(Math.sqrt(5.55));
+        System.out.println(Math.rint(5.55));
+        System.out.println((Math.random()*20));
+        System.out.println(Math.round(Math.random()*20));
+    }
+    
+}
