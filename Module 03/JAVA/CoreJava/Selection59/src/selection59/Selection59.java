5c1f47b1583748082fa86f92153ad5694eb9e2ac
thatsnajmul <thatsnajmul@yahoo.com>
2024-04-24 16:21:49 +0600

    Update

--- Module 03/JAVA/CoreJava/Selection59/src/selection59/Selection59.java
+++ Module 03/JAVA/CoreJava/Selection59/src/selection59/Selection59.java
@@ -6,12 +6,10 @@ package selection59;
 
 import java.util.Scanner;
 
-
 public class Selection59 {
 
- 
     public static void main(String[] args) {
- //--------------------------Find max number--------------------------------
+        //--------------------------Find max number--------------------------------
 //        Scanner scanner = new Scanner(System.in);
 //        int maxNumber;
 //        
@@ -38,16 +36,15 @@ public class Selection59 {
 //        
 //        System.out.println(maxNumber);
 
+
+
+//-----------------------------------Math random number-------------------------
 //        int randNumber = (int) (Math.random()*100);
 //        System.out.println(randNumber);
-        
-        
-        
 //        Scanner scanner =new Scanner(System.in);
 ////        char grade;
 //        System.out.println("Enter your number");
-//        int marks= scanner.nextInt();
-//        
+//        int marks= scanner.nextInt();   
 //        if(marks>100 || marks<0){
 //            System.out.println("Number is wrong");
 //        }
@@ -69,19 +66,74 @@ public class Selection59 {
 //        else{
 //            System.out.println("F");
 //        }
-
+//      System.out.println(marks);   
+//-------------------------------------Day number ------------------------------
 //        Scanner scanner = new Scanner(System.in);
-//        int maxNum;
+//        System.out.println("Please enter Day number:");
+//        int numberOfDay = scanner.nextInt();
+//
+//        String dayName = "";
+//
+//        switch (numberOfDay) {
+//            case 1:
+//                dayName = "Saturday";
+//                break;
+//
+//            case 2:
+//                dayName = "Sunday";
+//                break;
+//            case 3:
+//                dayName = "Monday";
+//                break;
+//            case 4:
+//                dayName = "Tuesday";
+//                break;
+//            case 5:
+//                dayName = "Wednesday";
+//                break;
+//            case 6:
+//                dayName = "Thirsday";
+//                break;
+//            case 7:
+//                dayName = "Friday";
+//                break;
+//
+//            default:
+//                dayName = "Invaid";
+//        }
 //        
-//        System.out.println("Enter your 1st number");
-//        int firstNum = scanner.nextInt();
-//        System.out.println("Enter your 2nd number");
-//        int secoendNum = scanner.nextInt();
-//        System.out.println("Enter your 3rd number");
-//        int thirdNum = scanner.nextInt();
-//        
-////        if(){
-////        }
+//        System.out.println(dayName);
+//---------------------------------A Simple calculator--------------------------
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your first number");
+        float number1 = scanner.nextFloat();
+        System.out.println("Enter your secoend number");
+        float number2 = scanner.nextFloat();
+        System.out.println("Add(+)\n Substraction(-)\npress 2\n Multiplication(X)\n Division(/)");
+        int choice = scanner.nextInt();
+
+        float result = 0.0f;
+
+        switch (choice) {
+            case 1:
+                result = number1 + number2;
+                break;
+            case 2:
+                result = number1 + number2;
+                break;
+            case 3:
+                result = number1 + number2;
+                break;
+            case 4:
+                result = number1 + number2;
+                break;
+            default:
+                System.out.println("Invalid");
+        }
+
+        System.out.println("Result is" + result);
+
+//--------------------------------
     }
-    
+
 }
