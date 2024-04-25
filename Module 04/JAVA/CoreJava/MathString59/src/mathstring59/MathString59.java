76a29f33f88988f8affc44a78b860f92fe12761e
thatsnajmul <thatsnajmul@yahoo.com>
2024-04-25 19:01:24 +0600

    Update

--- Module 04/JAVA/CoreJava/MathString59/src/mathstring59/MathString59.java
+++ Module 04/JAVA/CoreJava/MathString59/src/mathstring59/MathString59.java
@@ -8,24 +8,52 @@ public class MathString59 {
 
     public static void main(String[] args) {
         
-        System.out.println(Math.abs(-2.50));
-        System.out.println(Math.random());
-        System.out.println(Math.max(50,10));
-        System.out.println(Math.min(50,10));
-        System.out.println(Math.round(5.54));
-        System.out.println(Math.ceil(5.54));
-        System.out.println(Math.floor(5.54));
-        System.out.println(Math.log10(5));
-        System.out.println(Math.pow(5,3));
-        System.out.println(Math.sqrt(64));
-        System.out.println(Math.sqrt(5.55));
-        System.out.println(Math.rint(5.55));
-        System.out.println((Math.random()*20));
-        System.out.println(Math.round(Math.random()*20));
+//        System.out.println(Math.abs(-2.50));
+//        System.out.println(Math.random());
+//        System.out.println(Math.max(50,10));
+//        System.out.println(Math.min(50,10));
+//        System.out.println(Math.round(5.54));
+//        System.out.println(Math.ceil(5.54));
+//        System.out.println(Math.floor(5.54));
+//        System.out.println(Math.log10(5));
+//        System.out.println(Math.pow(5,3));
+//        System.out.println(Math.sqrt(64));
+//        System.out.println(Math.sqrt(5.55));
+//        System.out.println(Math.rint(5.55));
+//        System.out.println((Math.random()*20));
+//        System.out.println(Math.round(Math.random()*20));
         
         
         
        //ASCII Number, Char(), upperlower, /n,t and many more cover after break
+//       char alphacheck1=(char)65;
+//       char alphacheck2=(char)66;
+//       char alphacheck3=(char)67;
+//       char alphacheck4=(char)68;
+//       char alphacheck5=(char)69;
+//       char alphacheck6=(char)70;
+//       char alphacheck7=(char)65;
+//       char alphacheck8=(char)65;
+//       char alphacheck9=(char)65;
+//       char alphacheck10=(char)65;
+//       char alphacheck11=(char)65;
+//       char alphacheck12=(char)65;
+//       char alphacheck13=(char)65;
+//       char alphacheck14=(char)65;
+//       char alphacheck15=(char)65;
+//       char alphacheck16=(char)65;
+//       char alphacheck17=(char)65;
+//       char alphacheck18=(char)65;
+//       char alphacheck19=(char)65;
+//       char alphacheck20=(char)65;
+//       char alphacheck21=(char)65;
+//       char alphacheck22=(char)65;
+//       char alphacheck23=(char)65;
+//       char alphacheck24=(char)65;
+//       char alphacheck25=(char)65;
+//       char alphacheck26=(char)65;
+//       
+//        System.out.println(alphacheck1+alphacheck2+alphacheck3+alphacheck4+alphacheck5+alphacheck6+alphacheck7+alphacheck8+alphacheck9+alphacheck10 +alphacheck1+alphacheck1+alphacheck1+alphacheck1+alphacheck1+alphacheck1);
     }
     
 }
--- Module 04/JAVA/CoreJava/testString59/src/teststring59/TestString59.java
+++ Module 04/JAVA/CoreJava/testString59/src/teststring59/TestString59.java
@@ -9,34 +9,71 @@ import java.util.Scanner;
 public class TestString59 {
 
     public static void main(String[] args) {
-        
-        
-        Scanner input = new Scanner(System.in);
-        
-        System.out.println("Please enter your email address");
-        String userEmail = input.next();
-        System.out.println("Enter your password");
-        String password= input.next();
-        String message = String.format("Welcome %ss to google", userEmail);
-        
-        if(userEmail.equalsIgnoreCase("java")){
-//            System.out.println("Welcome to java");
-              
-              //intentionally blank
-              //M(2016-2017)Nastrc equals.time("mi");
-              if(password.equals("Java123")){
-                  System.out.println(message);
-              }
-              else{
-                  System.out.println("Incorrect password"); 
-              }
-        
-        }
-        else{
-            System.out.println("Couldn't find your google account");
-        }
-        
 
+        Scanner input = new Scanner(System.in);
+        //----------------------------------------------------------
+//        System.out.println("Please enter your email address");
+//        String userEmail = input.next();
+//        String message = String.format("Welcome %ss to google", userEmail);
+//        
+//        if(userEmail.equalsIgnoreCase("java")){
+////            System.out.println("Welcome to java");
+//              System.out.println("Enter your password");
+//              String password= input.next();
+//              //intentionally blank
+//              //M(2016-2017)Nastrc equals.time("mi");
+//              if(password.equals("Java123")){
+//                  System.out.println(message);
+//              }
+//              else{
+//                  System.out.println("Incorrect password"); 
+//              }
+//        
+//        }
+//        else{
+//            System.out.println("Couldn't find your google account");
+//        }
+
+//------------------------------------------------------------------------------
+        System.out.println("Enter Your username:");
+        String userName = input.next();
+        System.out.println("Enter Your password:");
+        String password = input.next();
+        String message = String.format("Welcome %ss to google", userName);
+        if (userName.equalsIgnoreCase("") && password.contentEquals("Java123")) {
+            System.out.println(message);
+//            if(userName.length()>=3){
+//                System.out.println("3 or more...");
+//            }
+//            else{
+//                System.out.println("3 or more...");
+//            }
+        } else {
+            System.out.println("Invalid");
+        }
+
+//------------------------------------------------------------
+//        System.out.println("Enter your username");
+//        String userName= input.nextLine();
+//        System.out.println(userName);
+
+
+//--------------------------------------------------------------
+
+// Ascii to find 
+//           System.out.println("Enter your latter");
+////         char userInput = (char)65;
+//           String word = input.;
+//         
+//           
+//           
+//           if(userInput.){
+//               System.out.println("Isn't Vaouel");  
+//           }
+//           else{
+//             System.out.println("Isn't Vaouel");   
+//           }
+           
     }
-    
+
 }
