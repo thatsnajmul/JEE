1c25371d43a1dd0a89647e1980e873352913c8b0
thatsnajmul <thatsnajmul@yahoo.com>
2024-04-30 16:54:38 +0600

    Update not ok

---
+++ Module 04/JAVA/CoreJava/RandomPassGenerator/src/randompassgenerator/RandomPassGenerator.java
@@ -0,0 +1,62 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package randompassgenerator;
+
+import java.util.Random;
+import java.util.Scanner;
+
+
+public class RandomPassGenerator {
+
+  
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        
+        
+        System.out.println("Enter password length");
+        int passLength=s.nextInt();
+        
+        
+        String pass ="";
+        String finalPass="";
+        
+        for(int i=1; i <=8; i++){
+            int range = random.nextInt(3)+1;
+            switch (range) {
+            case 1:
+                pass +=(char)(random.nextInt(26)+65);
+                break;
+            case 2:
+                pass +=(char)(random.nextInt(26)+97);
+                break;
+            case 3:
+                pass +=(char)(random.nextInt(26)+48);
+                break;
+        }
+        }
+        
+        for(int i =1; i<8; i++){
+            //pass.length()/2
+            if(i==4){
+                finalPass +="_"+pass.charAt(i);
+            }
+            else{
+                finalPass +=pass.charAt(i); //pass=ABCD__1234
+            }
+        }        
+        System.out.println(finalPass);
+        
+    }
+    
+    public static String passGenerator(int passLength){
+        Random random = new Random();
+        
+        return finalPass;
+    }
+    
+    
+    
+    
+}
