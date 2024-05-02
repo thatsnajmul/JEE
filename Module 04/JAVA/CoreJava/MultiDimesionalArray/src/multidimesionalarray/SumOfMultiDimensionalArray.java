b617122cfc8ee2ce78ac347e582c3d7d9da57707
thatsnajmul <thatsnajmul@yahoo.com>
2024-05-02 18:40:20 +0600

    Update Sum of Multi dimensional array
    Update Sum of Multi dimensional array

---
+++ Module 04/JAVA/CoreJava/MultiDimesionalArray/src/multidimesionalarray/SumOfMultiDimensionalArray.java
@@ -0,0 +1,28 @@
+
+package multidimesionalarray;
+
+
+public class SumOfMultiDimensionalArray {
+    
+    public static void main(String[] args) {
+        
+        int[][] numbers = {{10,20,30},{10,20,30},{10,20,30}};
+        
+        int sum = doSumofMdarray(numbers);
+        
+        System.out.println(sum);
+    }
+    
+    public static int doSumofMdarray(int[][] myArrays) {
+        int sum = 0; //
+        for(int row=0; row<myArrays.length; row++){
+            for(int col=0; col<myArrays[0].length; col++){
+                sum += myArrays[row][col];
+            }
+        }
+        return sum;
+    }
+    
+}
+
+
