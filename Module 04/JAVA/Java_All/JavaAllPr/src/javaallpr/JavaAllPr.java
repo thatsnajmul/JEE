d40593d40f74c52a5ca9463c1d73badb61f24721
thatsnajmul <thatsnajmul@yahoo.com>
2024-05-07 11:46:35 +0600

    Try OOP to the Main class and create a class
    Try OOP to the Main class and create a class that return class object value and newly assign to main class

--- Module 04/JAVA/Java_All/JavaAllPr/src/javaallpr/JavaAllPr.java
+++ Module 04/JAVA/Java_All/JavaAllPr/src/javaallpr/JavaAllPr.java
@@ -2,16 +2,38 @@
 //Main Package
 package javaallpr;
 
+import java.util.Arrays;
+
 
 
 //Main Class
 public class JavaAllPr {
+
+    public JavaAllPr() {
+    }
 
     
     
     //Main Method
     public static void main(String[] args) {
         
+        Student s = new Student();
+        s.name="Md Najmul Islam";
+        s.age=25;
+        s.height=5.5f;
+        s.weight=58.5f;
+        s.country="Bangladesh";
+        
+        System.out.println("My Name is "+s.name+" age is "+s.age+", height is "+s.height+", weight is "+s.weight+"and country is "+s.country);
     }
     
+    
+}
+
+class Student{
+    String name = "Abid";
+    int age;
+    float height;
+    float weight;
+    String country;
 }
