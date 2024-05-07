007ce6fc7e9774210122225e059632172a1b405c
thatsnajmul <thatsnajmul@yahoo.com>
2024-05-07 11:48:39 +0600

    Again commit

--- Module 04/JAVA/Java_All/JavaAllPr/src/javaallpr/JavaAllPr.java
+++ Module 04/JAVA/Java_All/JavaAllPr/src/javaallpr/JavaAllPr.java
@@ -1,36 +1,31 @@
-
 //Main Package
 package javaallpr;
-
 import java.util.Arrays;
-
-
 
 //Main Class
 public class JavaAllPr {
 
     public JavaAllPr() {
-    }
-
-    
-    
+    }   
     //Main Method
     public static void main(String[] args) {
-        
+        //object declare
         Student s = new Student();
+        //assaing value
         s.name="Md Najmul Islam";
         s.age=25;
         s.height=5.5f;
         s.weight=58.5f;
         s.country="Bangladesh";
-        
+        //print statement
         System.out.println("My Name is "+s.name+" age is "+s.age+", height is "+s.height+", weight is "+s.weight+"and country is "+s.country);
     }
     
     
 }
-
+//class make
 class Student{
+    //datatype and variable declare
     String name = "Abid";
     int age;
     float height;
