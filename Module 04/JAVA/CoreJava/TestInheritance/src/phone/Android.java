7b909da991e2f2efa2ae595e1101ec7aefa90b57
thatsnajmul <thatsnajmul@yahoo.com>
2024-05-08 15:22:23 +0600

    Update Inheritance

---
+++ Module 04/JAVA/CoreJava/TestInheritance/src/phone/Android.java
@@ -0,0 +1,14 @@
+
+package phone;
+
+//Variable declare
+//Default constractor or relational constractor
+//Argument constractor select 2
+//Getter and Setter
+//To String (Override)
+//Override
+public class Android {
+    String camera;
+    
+    
+}
--- Module 04/JAVA/CoreJava/TestInheritance/src/phone/Mobile.java
+++ Module 04/JAVA/CoreJava/TestInheritance/src/phone/Mobile.java
@@ -1,11 +1,69 @@
 
 package phone;
 
-
+//Supper Class
 public class Mobile {
+    //Variable declare
     private boolean makecall;
-    private boolean makecall;
+    private boolean makeText;
     private boolean camera;
-    private boolean makecall;
+    private boolean multimedia;
+    
+    //Default constractor or relational constractor
+     public Mobile() {
+    }
+    //Argument constractor select 2
+     public Mobile(boolean makecall, boolean message, boolean camera, boolean multimedia) {
+        this.makecall = makecall;
+        this.makeText = message;
+        this.camera = camera;
+        this.multimedia = multimedia;
+    }
+
+    //Getter and Setter
+    public boolean isMakecall() {
+        return makecall;
+    }
+
+    public void setMakecall(boolean makecall) {
+        this.makecall = makecall;
+    }
+
+    public boolean isMessage() {
+        return makeText;
+    }
+
+    public void setMessage(boolean message) {
+        this.makeText = message;
+    }
+
+    public boolean isCamera() {
+        return camera;
+    }
+
+    public void setCamera(boolean camera) {
+        this.camera = camera;
+    }
+
+    public boolean isMultimedia() {
+        return multimedia;
+    }
+
+    public void setMultimedia(boolean multimedia) {
+        this.multimedia = multimedia;
+    }
+
+    //To String
+    @Override
+    public String toString() {
+        return "Mobile{" + "makecall=" + makecall + ", message=" + makeText + ", camera=" + camera + ", multimedia=" + multimedia + '}';
+    }
+    //Override
+    
+    
+
+    
+   
+   
     
 }
