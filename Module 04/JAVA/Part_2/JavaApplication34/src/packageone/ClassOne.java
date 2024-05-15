9f1023a17d850cde71282b9c1c1d2c0468b148ca
thatsnajmul <thatsnajmul@yahoo.com>
2024-05-15 19:02:30 +0600

    Add

--- Module 04/JAVA/Part_2/JavaApplication34/src/packageone/ClassOne.java
+++ Module 04/JAVA/Part_2/JavaApplication34/src/packageone/ClassOne.java
@@ -1,21 +1,28 @@
 package packageone;
+
+import java.text.DateFormat;
+import java.util.LinkedList;
 import java.util.Queue;
+import java.util.TimeZone;
 
-public class ClassOne {    
-public static void main(String[] args) {
-Queue<String> queue = new java.util.LinkedList<>();
- queue.offer("Oklahoma");
- queue.offer("Indiana");
- queue.offer("Georgia");
- queue.offer("Texas");
+public class ClassOne {
 
- while (queue.size() > 3)
- System.out.println(queue.remove() + " Hello");
+    public static void main(String[] args) {
+        Queue<String> queue = new LinkedList<>();
+        queue.add("Allan");
+        queue.offer("Oklahoma");
+        queue.offer("Indiana");
+        queue.offer("Georgia");
+        queue.offer("Texas");
+        
+        TimeZone defaultTimeZone = null;
+
+        TimeZone defaultZone = defaultTimeZone; //no time generating without ....
+
+        while (queue.size() > 4) {
+            System.out.println("Hello, " + queue.remove() + "Your time is" + defaultTimeZone);
+        }
+
+    }
+
 }
-
-
- }
- 
-
-
-
