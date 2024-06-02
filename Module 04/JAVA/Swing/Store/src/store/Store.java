4981759117b50cbd9a373ce9c5a989688f0e187e
thatsnajmul <thatsnajmul@yahoo.com>
2024-06-02 22:07:53 +0600

    Add something

---
+++ Module 04/JAVA/Swing/Store/src/store/Store.java
@@ -0,0 +1,19 @@
+
+package store;
+
+import view.PrductView;
+
+
+public class Store {
+
+
+    public static void main(String[] args) {
+        
+        PrductView productview = new PrductView();
+        
+        productview.setVisible(true);
+        
+        
+    }
+    
+}
---
+++ Module 04/JAVA/Swing/Store/src/util/DbUtil.java
@@ -0,0 +1,29 @@
+
+package util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class DbUtil {
+    
+    private Connection con=null;
+    private String url="jdbc:mysql://localhost:3310/store";
+    private String user="root";
+    private String password="12345";
+    private String driver="com.mysql.cj.jdbc.Driver";
+    
+    public Connection getCon() throws ClassNotFoundException{
+        Class.forName(driver);
+        try {
+            con=DriverManager.getConnection(url, user, password);
+        } catch (SQLException ex) {
+            Logger.getLogger(DbUtil.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return con;
+    }
+   
+    
+}
