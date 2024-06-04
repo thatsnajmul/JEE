2cbe61a523e762ff3ada14c0cc6b9fe296f36e79
thatsnajmul <thatsnajmul@yahoo.com>
2024-06-05 05:39:49 +0600

    Hara

---
+++ Module 04/JAVA/hara/src/hara/Hara.java
@@ -0,0 +1,12 @@
+
+package hara;
+
+
+public class Hara {
+
+   
+    public static void main(String[] args) {
+       
+    }
+    
+}
---
+++ Module 04/JAVA/hara/src/hara/Pr3.java
@@ -0,0 +1,44 @@
+
+package hara;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+
+public class Pr3 {
+    
+    public static void main(String[] args) {
+//        Scanner sc=new Scanner(System.in);
+//            System.out.println("================================");
+            
+            //String s1=sc.next();
+            
+//            for(int i=1;i<3;i++)
+//            {
+//                int x=sc.nextInt();
+//                int y=sc.nextInt();
+//                int z=sc.nextInt();
+//                System.out.println("java               "+x);
+//                System.out.println("cpp               "+y);
+//                System.out.println("python               "+z);
+//                
+//                System.out.println("================================");
+//                //Complete this line
+//            }
+            
+            
+//             for (int i = 0; i < 3; i++) {
+//            String str = sc.next();
+//            int num = sc.nextInt();
+//            System.out.printf("%-15s%03d\n", str, num);
+//        }
+//        System.out.println("================================");
+
+
+            
+            
+            
+    }
+    
+}
