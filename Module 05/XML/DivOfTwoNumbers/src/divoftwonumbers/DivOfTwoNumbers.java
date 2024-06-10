5e06c24805776c04112f54b3789f15788e4f08cb
thatsnajmul <thatsnajmul@yahoo.com>
2024-06-10 10:28:19 +0600

    Add

---
+++ Module 05/XML/DivOfTwoNumbers/src/divoftwonumbers/DivOfTwoNumbers.java
@@ -0,0 +1,27 @@
+
+package divoftwonumbers;
+
+
+public class DivOfTwoNumbers {
+
+    
+    public static void main(String[] args) {
+        
+        int a;
+        int b;
+        int c;
+        
+        a=10;
+        b=50;
+        
+        c=b/a;
+        
+        
+        System.out.println(c);
+        
+        
+    }
+
+    
+    
+}
