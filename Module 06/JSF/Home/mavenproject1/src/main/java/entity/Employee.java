a78a2ac9e82ca0fb78d7e8def86bba53341467cd
thatsnajmul <thatsnajmul@yahoo.com>
2024-07-08 00:19:09 +0600

    Add

--- Module 06/JSF/Home/mavenproject1/src/main/java/entity/Employee.java
+++ Module 06/JSF/Home/mavenproject1/src/main/java/entity/Employee.java
@@ -15,5 +15,56 @@ public class Employee {
     private double salary;
 
     // Getters and setters and db then probably view and crud completely
+
+    public Employee() {
+    }
+
+    public Employee(Long id, String name, String department, double salary) {
+        this.id = id;
+        this.name = name;
+        this.department = department;
+        this.salary = salary;
+    }
+
+    public Employee(String name, String department, double salary) {
+        this.name = name;
+        this.department = department;
+        this.salary = salary;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+    
+    
+    
 }
 
