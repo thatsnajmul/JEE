e729c3e880065a114f45bcbec82798867c1e09a1
thatsnajmul <thatsnajmul@yahoo.com>
2024-07-09 18:30:50 +0600

    Add

--- Module 06/Angular/Class/myangular/src/app/app.module.ts
+++ Module 06/Angular/Class/myangular/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreatelocationComponent } from './createlocation/createlocation.component';
+import { StudentComponent } from './student/student.component';
+import { LocationComponent } from './location/location.component';
+import { EmployeeComponent } from './employee/employee.component';
+import { ModelComponent } from './model/location.model.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreatelocationComponent,
+    StudentComponent,
+    LocationComponent,
+    EmployeeComponent,
+    
   ],
   imports: [
     BrowserModule,
