396e1d951eae3865604db2384865499c8f36a33f
thatsnajmul <thatsnajmul@yahoo.com>
2024-07-10 00:01:52 +0600

    Add

---
+++ Module 06/Angular/Home/myangular/src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CreatelocationComponent } from './createlocation/createlocation.component';
+import { StudentComponent } from './student/student.component';
+import { LocationComponent } from './location/location.component';
+import { EmployeeComponent } from './employee/employee.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CreatelocationComponent,
+    StudentComponent,
+    LocationComponent,
+    EmployeeComponent,   
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideClientHydration()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
