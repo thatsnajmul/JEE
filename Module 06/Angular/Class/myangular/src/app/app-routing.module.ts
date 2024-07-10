077e7101f851255117de1715ea2b113348a145f2
thatsnajmul <thatsnajmul@yahoo.com>
2024-07-10 15:27:29 +0600

    Add

--- Module 06/Angular/Class/myangular/src/app/app-routing.module.ts
+++ Module 06/Angular/Class/myangular/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { StudentComponent } from './student/student.component';
+import { EmployeeComponent } from './employee/employee.component';
+import { LocationComponent } from './location/location.component';
+import { CreatelocationComponent } from './createlocation/createlocation.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path: 'student', component:StudentComponent},
+{path: 'employee', component:EmployeeComponent},
+{path: 'location', component:LocationComponent},
+{path: 'createlocation', component:CreatelocationComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
