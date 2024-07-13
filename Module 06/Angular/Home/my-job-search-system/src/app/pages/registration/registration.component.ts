53782769163fd1e8e108d4263d5a44c8d6df954d
thatsnajmul <thatsnajmul@yahoo.com>
2024-07-13 23:13:11 +0600

    Add

--- Module 06/Angular/Home/my-job-search-system/src/app/pages/registration/registration.component.ts
+++ Module 06/Angular/Home/my-job-search-system/src/app/pages/registration/registration.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { JobService } from '../../service/job.service';
 
 @Component({
   selector: 'app-registration',
@@ -6,5 +7,31 @@ import { Component } from '@angular/core';
   styleUrl: './registration.component.css'
 })
 export class RegistrationComponent {
+
+  employerObj: any = {
+    
+      "EmployerId": 0,
+      "CompanyName": "",
+      "EmailId": "",
+      "MobileNo": "",
+      "PhoneNo": "",
+      "CompanyAddress": "",
+      "City": "",
+      "State": "",
+      "PinCode": "",
+      "LogoURL": "",
+      "GstNo": ""
+  }
+
+  constructor(private job: JobService){
+    this.job.registrationEmployer(this.employerObj).subscribe((res:any)=>{
+      if(res.result){
+        alert('Save Successfully')
+      }
+      else{
+        alert('Save not Successfully')
+      }
+    })
+  }
 
 }
--- Module 06/Angular/Home/my-job-search-system/src/app/service/job.service.ts
+++ Module 06/Angular/Home/my-job-search-system/src/app/service/job.service.ts
@@ -1,9 +1,51 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+interface EmployeerData {
+  CompanyName: string;
+  EmailId: string;
+  MobileNo: string;
+  PhoneNo: string;
+  CompanyAddress: string;
+  City: string;
+  State: string;
+  PinCode: string;
+  LogoURL: string;
+  GstNo: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
+
+
 export class JobService {
 
-  constructor() { }
-}
+
+  private apiUrl = 'http://localhost:3000/AddNewEmployer';
+
+  constructor(private http: HttpClient) { }
+
+  registrationEmployer(employee: EmployeerData): Observable<EmployeerData> {
+    return this.http.post<EmployeerData>(this.apiUrl, employee);
+  }
+
+  // //apiEndPoint: string = 'https://freeapi.miniprojectideas.com/api/JobPortal/'
+  // baseUrl: string = "http://localhost:3000/AddNewEmployer";
+
+  // constructor(private http: HttpClient) { 
+    
+  // }
+
+  // registrationEmployer(obj: any){
+  //   //return this.http.post(this.apiEndPoint+'AddNewEmployer',obj);
+  //   //return this.httpClient.get(this.baseUrl);
+  //   return this.http.post(this.baseUrl,obj);
+  // }
+
+  
+  
+  }
+
+
