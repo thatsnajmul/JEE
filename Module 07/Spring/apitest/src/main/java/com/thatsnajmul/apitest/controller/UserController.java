b7b32fb3beba42a29da652e5d5c4f03956540ee7
thatsnajmul <thatsnajmul@yahoo.com>
2024-09-05 19:04:53 +0600

    Add

---
+++ Module 07/Spring/apitest/src/main/java/com/thatsnajmul/apitest/controller/UserController.java
@@ -0,0 +1,44 @@
+package com.thatsnajmul.apitest.controller;
+import com.thatsnajmul.apitest.entity.User;
+import com.thatsnajmul.apitest.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable Long id) {
+        return userService.getUserById(id);
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userService.saveUser(user);
+    }
+
+    @PutMapping("/{id}")
+    public User updateUser(@PathVariable Long id, @RequestBody User userDetails) {
+        User user = userService.getUserById(id);
+        user.setUsername(userDetails.getUsername());
+        user.setPassword(userDetails.getPassword());
+        user.setRole(userDetails.getRole());
+        return userService.saveUser(user);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+    }
+}
---
+++ Module 07/Spring/apitest/src/main/java/com/thatsnajmul/apitest/service/UserService.java
@@ -0,0 +1,38 @@
+package com.thatsnajmul.apitest.service;
+
+import com.thatsnajmul.apitest.entity.User;
+import com.thatsnajmul.apitest.userrepository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User getUserById(Long id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+    }
+}
