19a744e6d1415696973eb6e73ff71fd0282079bf
thatsnajmul <thatsnajmul@yahoo.com>
2024-09-24 16:39:25 +0600

    Add

--- Module 07/SpringbootwithAngular/Spring Boot/job-sys/src/main/java/com/thatsnajmul/job_sys/repository/UserRepository.java
+++ Module 07/SpringbootwithAngular/Spring Boot/job-sys/src/main/java/com/thatsnajmul/job_sys/repository/UserRepository.java
@@ -1,10 +1,18 @@
-//package com.thatsnajmul.job_sys.repository;
-//
-//import com.thatsnajmul.job_sys.model.User;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.stereotype.Repository;
-//
-//@Repository
-//public interface UserRepository extends JpaRepository<User, Long> {
-//    User findByUsername(String username);
-//}
+package com.thatsnajmul.job_sys.repository;
+
+
+import com.thatsnajmul.job_sys.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+
+
+
+}
--- Module 07/SpringbootwithAngular/Spring Boot/job-sys/src/main/java/com/thatsnajmul/job_sys/service/UserService.java
+++ Module 07/SpringbootwithAngular/Spring Boot/job-sys/src/main/java/com/thatsnajmul/job_sys/service/UserService.java
@@ -1,48 +1,25 @@
-//package com.thatsnajmul.job_sys.service;
-//
-//import com.thatsnajmul.job_sys.model.User;
-//import com.thatsnajmul.job_sys.repository.UserRepository;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.security.core.userdetails.UserDetails;
-//import org.springframework.security.core.userdetails.UserDetailsService;
-//import org.springframework.security.core.userdetails.UsernameNotFoundException;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//import org.springframework.stereotype.Service;
-//
-//import java.util.Optional;
-//
-//@Service
-//public class UserService implements UserDetailsService {
-//
-//    private final UserRepository userRepository;
-//    private final PasswordEncoder passwordEncoder;
-//
-//    @Autowired
-//    public UserService(UserRepository userRepository) {
-//        this.userRepository = userRepository;
-//        this.passwordEncoder = new BCryptPasswordEncoder();
-//    }
-//
-//    public User saveUser(User user) {
-//        user.setPassword(passwordEncoder.encode(user.getPassword()));
-//        return userRepository.save(user);
-//    }
-//
-//    public Optional<User> findByUsername(String username) {
-//        return Optional.ofNullable(userRepository.findByUsername(username));
-//    }
-//
-//    @Override
-//    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-//        User user = userRepository.findByUsername(username);
-//        if (user == null) {
-//            throw new UsernameNotFoundException("User not found");
-//        }
-//        return org.springframework.security.core.userdetails.User
-//                .withUsername(user.getUsername())
-//                .password(user.getPassword())
-//                .authorities("USER")
-//                .build();
-//    }
-//}
+package com.thatsnajmul.job_sys.service;
+
+import com.thatsnajmul.job_sys.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByEmail(username)
+                .orElseThrow(
+                        ()->  new UsernameNotFoundException("User Not Found With this Email Address"));
+    }
+
+
+}
